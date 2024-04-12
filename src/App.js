ed257ada33f23d4c9b6cbedf79e875067864dc23
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-04-12 21:26:30 +0200

    clickEvents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
-    <div className="steps">
-      Hello React
-      <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          "x"
+        ) : (
+          <div className="buttons button">
+            <button
+              style={{
+                backgroundColor: "#7950f2",
+                color: "#fff",
+                justifyContent: "center",
+              }}
+            >
+              Open
+            </button>
+          </div>
+        )}
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          Hello React
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons button">
+            <button
+              style={{
+                backgroundColor: "#7950f2",
+                color: "#fff",
+              }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
